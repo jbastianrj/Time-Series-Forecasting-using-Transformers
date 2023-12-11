7cd449e44e5bf41042e740aea74e3c268ff423ed
Sebastian Ramírez Jiménez <119964608+jbastianrj@users.noreply.github.com>
2023-12-11 04:05:05 -0600

    Pruebas.R
    The nonlinearity tests were performed with the terasvirta.test library of R. In Python it required a rather long implementation, so it was preferred to leave it in R in much fewer lines of code.

--- Pruebas.R
+++ Pruebas.R
@@ -40,10 +40,11 @@ terasvirta.test(ts)
 # Estandarizar la serie de tiempo
 ts_scaled <- scale(ts)
 terasvirta.test(ts_scaled)
-#p-value = 0.04015 >> NO LINEAL
+#p-value = 0.04015 >> NO LINEAl 
 
 #W03
 #Diferenciamos una vez y haces la prueba nuevamente.
+#ESCALAR DA EL MISMO P VALUE
 
 library('stats')
 ts_diff <- diff(ts,differences = 1)
@@ -62,4 +63,126 @@ terasvirta.test(na.omit(df_descompose_mult$random))
 
 #APLICACIONES CON OTRAS SERIES DE TIEMPO
 
+#W11
 #Airlane Passengers
+
+ts <- read.csv('airline.csv')
+ts <- ts(ts, frequency = 12)
+plot(ts)
+terasvirta.test(ts) #SI ES MUY CERCANO
+ts_scaled <- scale(ts) 
+terasvirta.test(ts_scaled) #SI ES MUY CERCANO
+ts_diff <- diff(ts,differences = 1)
+terasvirta.test(ts_diff) #NO
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #NO
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #NO 
+
+
+#W21
+#RESERVA FEDERAL
+
+ts <- read.csv('reserva.csv')
+ts <- ts(ts, frequency = 12)
+plot(ts)
+terasvirta.test(ts) #SI
+ts_scaled <- scale(ts) 
+terasvirta.test(ts_scaled) #SI
+ts_diff <- diff(ts,differences = 1)
+terasvirta.test(ts_diff) #NO
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #NO
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #NO 
+
+#W31
+#COLOMBIA DENGUE
+
+ts <- read.csv('colombia.csv')
+ts <- ts(ts, frequency = 52)
+plot(ts)
+terasvirta.test(ts) #NO
+ts_scaled <- scale(ts) 
+terasvirta.test(ts_scaled) #NO
+ts_diff <- diff(ts,differences = 1)
+terasvirta.test(ts_diff) #SI
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #SI
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #NO 
+
+
+#W41
+#BANGKOK DENGUE
+
+ts <- read.csv('bangkok.csv')
+ts <- ts(ts, frequency = 12)
+plot(ts)
+terasvirta.test(ts) #NO
+ts_scaled <- scale(ts) 
+terasvirta.test(ts_scaled) #NO
+ts_diff <- diff(ts,differences = 1)
+terasvirta.test(ts_diff) #NO
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #NO
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #SI
+
+
+#W51
+#JAPAN FLU
+
+ts <- read.csv('japan.csv')
+ts <- ts(ts, frequency = 52)
+plot(ts)
+terasvirta.test(ts) #NO
+ts_scaled <- scale(ts) 
+terasvirta.test(ts_scaled) #NO
+ts_diff <- ts #diff(ts,differences = 1) # NO SE DIFERENCIA QUEDA IGUAL
+terasvirta.test(ts_diff) #NO
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #SI
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #NO 
+
+#W61
+#NETFLIX STOCK 
+
+ts <- read.csv('NFLX.csv')
+ts <-as.data.frame(ts)
+ts <- ts(ts$Close, frequency = 260) #Días hábiles en un año
+plot(ts)
+terasvirta.test(ts) #SI
+ts_scaled <- scale(ts)
+ts_scaled <- ts(ts_scaled, frequency = 260)
+terasvirta.test(ts_scaled) #SI
+plot(ts_scaled)
+ts_diff <- diff(ts,differences = 2)
+terasvirta.test(ts_diff) #NO
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #SI
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #NO 
+
+
+
+#W71  
+#Sunspots Daily 
+
+ts <- read.csv('SunspotsDaily.csv', header = FALSE, sep = ';')
+plot(ts$V5)
+ts<-ts$V5[73874:74659]
+#Solo tomamos 265 datos
+plot(ts)
+ts <- ts(ts, frequency = 365) #Días
+terasvirta.test(ts) #SI
+ts_scaled <- scale(ts)
+ts_scaled <- ts(ts_scaled, frequency = 365)
+terasvirta.test(ts_scaled) #SI
+ts_diff <- diff(ts,differences = 1)
+terasvirta.test(ts_diff) #SI
+df_descompose_aditivo <- decompose(ts, type = 'additive')
+terasvirta.test(na.omit(df_descompose_aditivo$random)) #NO
+df_descompose_mult <- decompose(ts, type = 'multiplicative')
+terasvirta.test(na.omit(df_descompose_mult$random)) #NO 
